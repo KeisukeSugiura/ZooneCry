3b80b2354e4b444f5566c7cffbbb9c480c71486a
吉田圭佑 <yoshidakeisuke@Murzim.ics.nitech.ac.jp>
2017-06-14 13:48:38 +0900

    now test of using class

--- ZooneCryModule.py
+++ ZooneCryModule.py
@@ -1,10 +1,42 @@
+# coding: utf-8
+
 ###############################
 # ZooneCryModule : 動物の鳴き声認識共通スクリプト
 # developed by Keisuke Yoshida
 ###############################
 
 # import library
+import wave
 import numpy as np
 import scipy.signal
 import scipy.fftpack
 import scipy.fftpack.realtransforms
+
+class ZooneCryModule: 
+	name = "abc"
+	dataLength = 10
+	dataDirectry = "./data/wav/"
+	classNameArray = ["dog", "cat"]
+
+
+	def __init__(self, name):
+		self.name = name
+
+	def getName(self):
+		return self.name
+
+	def getClassNameArray(self):
+		return self.classNameArray
+
+	def getDataLength(self):
+		return self.dataLength
+
+
+
+
+# test methods
+# if __name__ == '__main__':
+# 	test = ZooneCryModule("test")
+# 	print(test.getName())
+# 	print(test.getClassNameArray())
+# 	print(test.getDataLength())
